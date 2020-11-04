ed970bc5a36b22e0410dbe13cc059904f088de57
Biringa Chidera <biringachidera@gmail.com>
2020-11-04 15:41:24 -0500

    added KeywordExtractor class

--- features/extractors/lexical_extractors.py
+++ features/extractors/lexical_extractors.py
@@ -1,36 +1,45 @@
 import os
 import sys
+from typing import List
 import pandas as pd
-import utils
+import utils as ut
+from math import floor, log
 
-print(utils.java_keywords)
 
-
-class LexicalExtractor:
-	"""Extracts lexical features"""
-	def __init__(self, path, java_keywords):
+class KeywordExtractor:
+	"""Extracts frequency of keywords"""
+	def __init__(self, path):
 		self.path = path
-		self.java_keywords = None # from import
+		self.java_keywords = ut.java_keywords
 
-	def process_data(self) -> list[str]:
+	def process_data(self) -> List[int]:
 		# Reads in our dataset and extracts sourcecode column
-		dataset = pd.read_csv("dataset.csv")
+		dataset = pd.read_csv(f"{self.path}/dataset.csv")
 		source_code = [file for file in dataset["sourcecode"]]
 		return source_code
 
-
-	def character_count(self) -> int:
-		# counts the number of characters in a source code
-		character_count = [len(file) for file in self.process_data()]
-		return character_count
+	def get_character_frequency(self) -> List[int]:
+		# gets the frequency of characters
+		character_frequency = [len(file) for file in self.process_data()]
+		return character_frequency
 	
-	def keyword_featues(self) -> int:
-		# counts the number of characters in a file
-		pass
+	def get_keyword_frequency(self) -> List[int]:
+		# gets the frequency of keywords
+		keyword_frequency = []
+		for file in self.process_data():
+			temp = [word for word in file.split() if word in self.java_keywords]
+			keyword_frequency.append(len(temp))
+		return keyword_frequency
 	
-	def extract(self) -> int:
-		pass
+	def extract_keywords(self) -> List[int]:
+		# extract keyword features
+		lexical_features = []
+		for cf, kf in zip(self.get_character_frequency(), self.get_keyword_frequency()):
+			lexical_features.append(floor(log(cf / kf)))
+		return lexical_features
 
 
 if __name__ == "__main__":
-	pass
+	path = "/Users/Gabriel/Documents/research/experimentaion/dataset_creator"
+	le = KeywordExtractor(path)
+	print(le.extract_keywords()) 
