41c4d754171a44207b3705188453ec452642fb06
Biringa Chidera <biringachidera@gmail.com>
2020-11-03 23:06:17 -0500

    update dataset_creator

--- dataset_creator/dataset_creator.py
+++ dataset_creator/dataset_creator.py
@@ -5,6 +5,10 @@ import csv
 
 # Usage: dataset_creator.py [-h] [-v VERSION] [-H HASH] [-b BUILD] [-t TEST]
 
+# Step 1: Call "create_csv_file" method without argument  
+# Step 2: comment out "create_csv_file" method
+# Step 3: Call "populate_csv_file" with argument
+
 parser = argparse.ArgumentParser(description = "Creates datasets")
 parser.add_argument("-v", "--version", help = "Repository version")
 parser.add_argument("-H", "--hash", help = "Repository commit hash value")
@@ -14,29 +18,34 @@ args = parser.parse_args()
 
 class DatasetCreator:
 	"""Create a dataset using GitHub repositories"""
-	def __init__(self, path):
+	def __init__(self, path, filename, version, hash, build, test):
 		self.fieldnames  = ["version", "hash", "sourcecode", "build", "test"]
 		self.path = path
+		self.filename = filename
+		self.version = version
+		self.hash = hash
+		self.build = build
+		self.test = test
 
 	def create_csv_file(self) -> None:
-		with open("dataset.csv", "w") as dataset:
+		with open(f"{self.filename}.csv", "w") as dataset:
 			writer = csv.DictWriter(dataset, fieldnames = self.fieldnames)
 			writer.writeheader()
 
-	def populate_csv_file(self, version, hash, build, test) -> None:		
+	def populate_csv_file(self) -> None:		
 		for (root, subdirectories, files) in os.walk(self.path):
 			for file in files:
 				if file.endswith(".java"):
 					temp = os.path.join(root, file)
 					try: 
 						with open(temp, "r") as java_file:
-							with open("dataset.csv", "a") as dataset:
+							with open(f"{self.filename}.csv", "a") as dataset:
 								writer = csv.DictWriter(dataset, fieldnames = self.fieldnames)
-								writer.writerow({"version" : version,
-												"hash" : hash, 
+								writer.writerow({"version" : self.version,
+												"hash" : self.hash, 
 												"sourcecode" : java_file.read(),
-												"build" : build, 
-												"test" : test
+												"build" : self.build, 
+												"test" : self.test
 												})
 					except Exception as e:
 						print(e)
@@ -44,7 +53,8 @@ class DatasetCreator:
 												
 
 if __name__ == '__main__':
-	path = "/Users/Gabriel/Documents/research/data_source/sc1/addressbook-level2-5.0"
-	dc = DatasetCreator(path)
+	path = "/Users/Gabriel/Documents/research/data_source/sc1/addressbook-level2-1.0"
+	filename = "dataset"
+	dc = DatasetCreator(path, filename, args.version, args.hash, args.build, args.test)
 	# dc.create_csv_file()
-	# dc.populate_csv_file(args.version, args.hash, args.build, args.test)
+	# dc.populate_csv_file()
