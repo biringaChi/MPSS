37849d157ffa1d31e4dd1aa71c1973793f483183
Biringa Chidera <biringachidera@gmail.com>
2020-11-05 13:28:00 -0500

    update to process_data

--- features/extractors/lexical/process_data.py
+++ features/extractors/lexical/process_data.py
@@ -3,12 +3,17 @@ import pandas as pd
 
 class ProcessData:
 	"""Processes data from dataset file"""
-	def __init__(self, path) -> None:
-		self.path = path
+	def __init__(self) -> None:
+		# Figure out a way to walk up directory without absolutely specifying path
+		self.path = "/Users/Gabriel/Documents/research/experimentaion/dataset_creator/dataset.csv"
 		
 	def __repr__(self) -> str:
 		return f"{self.__class__.__name__}(self.path)"
 
 	def process(self) -> List[int]:
-		dataset = pd.read_csv(f"{self.path}/dataset.csv")
+		dataset = pd.read_csv(self.path)
 		return [file for file in dataset["sourcecode"]]
+	
+	def get_character_frequency(self) -> List[int]:
+		return [len(file) for file in self.process()]
+	
