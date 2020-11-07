03df518b339d4b9a693dd34e43d31e81fabeb72c
Biringa Chidera <biringachidera@gmail.com>
2020-11-06 21:11:37 -0500

    update keywords

--- features/extractors/lexical/keywords.py
+++ features/extractors/lexical/keywords.py
@@ -1,5 +1,8 @@
-from utils import java_keywords
-from  process_data import ProcessData
+import os
+import sys
+import _specify_dir
+from features.extractors.process_data import ProcessData
+from features.extractors.utils import java_keywords
 from typing import List
 from math import floor, log10
 
@@ -12,20 +15,21 @@ class KeywordExtractor(ProcessData):
 	
 	def __repr__(self) -> str:
 		return f"Class: {self.__class__.__name__}"
-	
+
 	def __str__(self) -> str:
 		return f"Java keywords: {self.java_keywords}"
-	
+
+	@property
 	def get_keyword_frequency(self) -> List[int]:
 		keyword_frequency = []
-		for file in self.process():
+		for file in self.sourcecode:
 			temp = [word for word in file.split() if word in self.java_keywords]
 			keyword_frequency.append(self.__len__(temp))
 		return keyword_frequency
 	
 	def extract_keywords(self) -> List[int]:
 		lexical_features = []
-		for cf, kf in zip(self.get_character_frequency(), self.get_keyword_frequency()):
+		for cf, kf in zip(self.get_character_frequency, self.get_keyword_frequency):
 			try:
 				lexical_features.append(floor(log10(cf / kf)))
 			except ZeroDivisionError:
