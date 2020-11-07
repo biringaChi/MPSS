a9baf19f7df229680c023edf8fe42fe1ee91a4e2
Biringa Chidera <biringachidera@gmail.com>
2020-11-06 23:23:17 -0500

    update process data

--- features/extractors/process_data.py
+++ features/extractors/process_data.py
@@ -1,12 +1,13 @@
-from typing import List
+import os
+import sys
 import pandas as pd
+from typing import List
 
 class ProcessData:
 	"""Processes data from dataset file"""
 	def __init__(self) -> None:
-		# Figure out a way to walk up directory without absolutely specifying path
-		self.path = "/Users/Gabriel/Documents/research/experimentation/dataset_creator/dataset.csv"
-		
+		pass
+	
 	def __repr__(self) -> str:
 		return f"{self.__class__.__name__}(self.path)"
 	
@@ -14,8 +15,15 @@ class ProcessData:
 		return len(value)
 
 	@property
+	def _get_data_path(self) -> str:
+		walk_up = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+		subfolders = [subs.path for subs in os.scandir(walk_up) if subs.is_dir()]
+		data_file_path = os.path.join([subs.path for subs in os.scandir(walk_up) if subs.is_dir()][-1], os.listdir(subfolders[-1])[-1])
+		return data_file_path
+
+	@property
 	def sourcecode(self) -> List[int]:
-		dataset = pd.read_csv(self.path)
+		dataset = pd.read_csv(self._get_data_path)
 		return [file for file in dataset["sourcecode"]]
 	
 	@property
