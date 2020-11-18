187b4e3ae5cfabceec513803189acb88fb4385d3
Biringa Chidera <biringachidera@gmail.com>
2020-11-18 16:47:17 -0500

    update to ASTs

--- FeatureEng/Extractors/Syntactic/src/ASTs.java
+++ FeatureEng/Extractors/Syntactic/src/ASTs.java
@@ -16,7 +16,7 @@ import com.google.common.collect.Iterators;
 import com.opencsv.exceptions.CsvValidationException;
 
 public class ASTs extends ASTPrep {
-
+	
 	public List<String> viewASTs() throws CsvValidationException, IOException {
 		YamlPrinter printer = new YamlPrinter(true);
 		List<String> ASTs = new ArrayList<>();
@@ -40,21 +40,21 @@ public class ASTs extends ASTPrep {
 		} else System.err.println("Please provide a valid compilation unit idx");
 	}
 
-	public List<Integer> numNodes() throws CsvValidationException, IOException {
-		List<Integer> nodes = new ArrayList<>();
+	public List<Double> numNodes() throws CsvValidationException, IOException {
+		List<Double> nodes = new ArrayList<>();
 		for (CompilationUnit ast : getASTs()) {
 			if (ast != null) {
 				PreOrderIterator iterator = new Node.PreOrderIterator(ast);
 				int size = Iterators.size(iterator);
-				nodes.add(size);
-			} else nodes.add(0);
+				nodes.add((double) (Math.round(Math.log10(size) * 100.0) / 100.0));
+			} else nodes.add((double) 0);
 		}
 		return nodes;
 	}
 
 	public int maxDepth() throws CsvValidationException, IOException {
 		CompilationUnit ast = getASTs().get(0);
-		List<List<String>> result = new ArrayList<>();
+		List<List<String>> levels = new ArrayList<>();
 		Node root = ast.findRootNode();
 		if(root == null) return 0;
 		Queue<Node> queue = new LinkedList<>();
@@ -70,34 +70,28 @@ public class ASTs extends ASTPrep {
 				if(currentNode.getChildNodes().get(1) != null) queue.add(currentNode.getChildNodes().get(1));
 			}
 			depth++;
-			result.add(level);
+			levels.add(level);
 		}
 		return depth;
 	}
 
-	public HashMap<String, List<String>> nodeType() throws CsvValidationException, IOException {
-		List<String> temp = new ArrayList<>();
-		HashMap<String, List<String>> nodeTypes = new HashMap<>(); 
-		String d = "doc";
+	public HashMap<String, List<String>> treeNodeTypes() throws CsvValidationException, IOException {
 		Integer idx = 0;
-		for(String doc : viewASTs()) {
-			if(doc != null) {
-				String[] words = doc.split("\\s+");
-				for(String word : words) {
-					if(word.indexOf("Type=") != -1) {
-						temp.add(word.substring(word.indexOf('=') + 1, word.indexOf(')')));
+		String docId = "doc", typeId = "Type="; 
+		HashMap<String, List<String>> treeNodeType = new HashMap<>(); 
+		for(String docNode : viewASTs()) {
+			List<String> temp = new ArrayList<>();
+			if(docNode != null) {
+				String[] nodes = docNode.split("\\s+");
+				for(String node : nodes) {
+					if(node.indexOf(typeId) != -1) {
+						temp.add(node.substring(node.indexOf('=') + 1, node.indexOf(')')));
 					}
 				}
-				nodeTypes.put(d + idx, temp);
-			} else nodeTypes.put(d + idx, null);
+				if (!treeNodeType.containsKey(docId + idx)) treeNodeType.put(docId + idx, temp);
+			} else treeNodeType.put(docId + idx, null);
 			idx++;
 		}
-		return nodeTypes;
-	}
-
-	public void nodeTypeTF() {
-	}
-
-	public void nodeTypeTFIDF() {
+		return treeNodeType;
 	}
 }
